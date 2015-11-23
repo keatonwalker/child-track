e5c7f3498a89d4fd9506d86b5610eb9ddcf4196b
kwalker9 <klwalker9@gmail.com>
2015-11-22 17:51:18 -0700

    Added Child profile screen and children collection.

---
+++ client/templates/application/child_profile.js
@@ -0,0 +1,6 @@
+Template.childProfile.helpers({
+    gender: function() {
+        var a = this.isMale ? "male" : "female";
+        return a
+    }
+});
---
+++ server/fixtures.js
@@ -0,0 +1,9 @@
+if (Children.find().count() === 0) {
+    Children.insert({
+        firstName: "Finn",
+        middleName: "Ashton",
+        lastName: "Walker",
+        birthdate: "4/20/13",
+        isMale: "true"
+    });
+}
